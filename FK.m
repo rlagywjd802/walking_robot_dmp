e87b6348cc125ac92b321ccfd523f721772bd3bc
Hyojeong <rlagywjd802@gmail.com>
2018-10-23 18:37:34 -0700

    Added Joint Space dmp

--- FK.m
+++ FK.m
@@ -7,11 +7,18 @@ function [x] = FK(q)
     global l1
     global l2
     
-    q = q*D2R;    
-    x1 = l1*sin(q(:,1));
-    y1 = -l1*cos(q(:,1));
-    x2 = x1 + l2*sin(q(:,1)-q(:,2));
-    y2 = y1 - l2*cos(q(:,1)-q(:,2));
+    q1 = 3*pi/2 + q(:, 1)*D2R;
+    q2 = - q(:, 2)*D2R;
+    x1 = l1*cos(q1);
+    y1 = l1*sin(q1);
+    x2 = x1 + l2*cos(q1+q2);
+    y2 = y1 + l2*sin(q1+q2);
+    
+%     q = q*D2R;
+%     x1 = l1*sin(q(:,1));
+%     y1 = -l1*cos(q(:,1));
+%     x2 = x1 + l2*sin(q(:,1)-q(:,2));
+%     y2 = y1 - l2*cos(q(:,1)-q(:,2));
     
     x = [x2, y2];
 end
